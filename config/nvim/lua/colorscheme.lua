bd1137410760af1c4b4cc640c5f3931148ef8b0e
阿五 <hugewilliam@foxmail.com>
2023-09-10 19:58:15 +0800

    feat: remove neovim's config

--- config/nvim/lua/colorscheme.lua
+++
@@ -1,9 +0,0 @@
-local colorscheme = 
-"tokyonight"
--- "dracula"
-local status_ok, _ = pcall(vim.cmd, "colorscheme" .. " " .. colorscheme)
-
-if not status_ok then
-  vim.notify("colorscheme" .. " " .. colorscheme .. "is not found!")
-  return
-end
